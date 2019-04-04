882449c686d09142991b33ccbbfcd4ae837a7bc4
Carmine <cgrossi@gmail.com>
2019-04-04 17:51:36 +0800

    wired numbered buttons

--- scripts.js
+++ scripts.js
@@ -26,3 +26,47 @@ function operate(operator, a, b) {
     }
 }
 
+const btn = document.querySelectorAll('.btn')
+const display = document.querySelector('.display')
+let first = '';
+let second = '';
+let displayValue = '';
+
+const btnClick = btn.forEach(function(el) {
+    if(!el.classList.contains('display')) {
+        el.addEventListener('click', function(e) {
+            console.log(typeof display.innerHTML)
+            if(display.innerHTML === '0') {
+                display.innerHTML = parseInt(e.target.innerHTML);
+                displayValue += parseInt(e.target.innerHTML)
+            } else {
+                console.log('else')
+                display.innerHTML += parseInt(e.target.innerHTML)
+                displayValue += parseInt(e.target.innerHTML)
+            }
+        })
+    }
+})
+
+
+
+
+
+
+// const one = document.querySelector('.one')
+// const two = document.querySelector('.two')
+// const three = document.querySelector('.three')
+// const four = document.querySelector('.four')
+// const five = document.querySelector('.five')
+// const six = document.querySelector('.six')
+// const seven = document.querySelector('.seven')
+// const eight = document.querySelector('.eight')
+// const nine = document.querySelector('.nine')
+// const zero = document.querySelector('.zero')
+// const clear = document.querySelector('.clear')
+// const add = document.querySelector('.add')
+// const subtract = document.querySelector('.subtract')
+// const multiply = document.querySelector('.multiply')
+// const divide = document.querySelector('.divide')
+// const equals = document.querySelector('.equals')
+
