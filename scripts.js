b30859b122df3e5d62da088381775ad9f483a846
Carmine <cgrossi@gmail.com>
2019-04-05 16:14:33 +0800

    added decimal fixed rounding

--- scripts.js
+++ scripts.js
@@ -4,12 +4,19 @@ const opBtn = document.querySelectorAll('.operator')
 const display = document.querySelector('.display')
 const clearBtn = document.querySelector('.clear')
 const equalsBtn = document.querySelector('.equals')
+const backBtn = document.querySelector('.backspace')
+const decimalBtn = document.querySelector('.decimal')
 
+
+let numArr = [];
+let opArr = [];
+let total = 0;
 let firstNum = '';
 let secondNum = '';
 let operation = '';
 let displayValue = '';
 let evaluated = false;
+let stop = false;
 
 function add(a, b) {
     return a + b;
@@ -24,7 +31,12 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b;
+    if(b > 0) {
+        return a / b;
+    } else {
+        return 'Oh, a wise guy, eh?';
+    }
+
 }
 
 function operate(operator, a, b) {
@@ -39,6 +51,23 @@ function operate(operator, a, b) {
     }
 }
 
+function backspace() {
+    if(display.innerHTML.length > 1) {
+        display.innerHTML = display.innerHTML.substring(0, display.innerHTML.length-1);
+        displayValue = displayValue.substring(0, displayValue.length-1)
+    } else {
+        display.innerHTML = 0;
+        displayValue = '';
+    }
+}
+
+function decimal() {
+    if(displayValue.indexOf('.') < 0) {
+        displayValue = `${displayValue}.`;
+        display.innerHTML = `${display.innerHTML}.`;
+    }
+}
+
 function resetValues() {
     firstNum = '';
     secondNum = '';
@@ -48,51 +77,106 @@ function resetValues() {
 
 function clearDisplay() {
     display.innerHTML = '0';
+    display.setAttribute('style', '');
+    stop = false;
     resetValues();
 }
 
 function renderNumbers(numClicked) {
-    if(display.innerHTML === '0' || evaluated) {
-        display.innerHTML = parseInt(numClicked.target.innerHTML);
-        if(evaluated) {
-            evaluated = false;
-            resetValues();
+    if(!stop) {
+        if(display.innerHTML === '0' || evaluated) {
+            display.innerHTML = parseInt(numClicked.target.innerHTML);
+            if(evaluated) {
+                evaluated = false;
+                resetValues();
+                }
+            displayValue += parseInt(numClicked.target.innerHTML);
+        } else {
+            if(display.innerHTML.length >= 16) {
+                display.setAttribute('style', 'font-size: 2.5rem;');
+                if(display.innerHTML.length >= 40) {
+                    display.innerHTML = 'Stop screwing with the calculator!';
+                    stop = true;
+                    return;
+                }
+            } else {
+                display.setAttribute('style', '')
             }
-        displayValue += parseInt(numClicked.target.innerHTML);
-    } else {
-        display.innerHTML += parseInt(numClicked.target.innerHTML);
-        displayValue += parseInt(numClicked.target.innerHTML);
+            display.innerHTML += parseInt(numClicked.target.innerHTML);
+            displayValue += parseInt(numClicked.target.innerHTML);
+        }
     }
-}
-
-function preEvaluate() {
-    secondNum = display.innerHTML.substring(display.innerHTML.lastIndexOf(' ') + 1);
-    operation = display.innerHTML.match(/[/+\-*]/)[0];
-}
-
+};
 
 numBtn.forEach(function(el) {
     el.addEventListener('click', function(e) {
         renderNumbers(e);
     });
-})
+});
 
 opBtn.forEach(function(el) {
     el.addEventListener('click', function(e) {
-        firstNum = displayValue;
-        display.innerHTML += ` ${e.target.innerHTML} `;
+        if(!display.innerHTML.charAt(-1)) {
+            numArr.push(displayValue)
+            opArr.push(e.target.innerHTML);
+            display.innerHTML += ` ${e.target.innerHTML} `;
+            displayValue = '';
+            total++;
+        }
     });
-})
+});
 
 clearBtn.addEventListener('click', function() {
     clearDisplay();
-})
+});
 
 equalsBtn.addEventListener('click', function() {
-    preEvaluate();
-    const parsedFirst = Number.parseInt(firstNum, 10);
-    const parsedSecond = Number.parseInt(secondNum, 10);
-    display.innerHTML = operate(operation, parsedFirst, parsedSecond);
-    evaluated = true;
-})
+    if(!display.innerHTML.match(/\d$/).length < 1) {
+        numArr.push(displayValue);
+        let first = undefined;
+        let second = undefined;
+        let operator = undefined;
+        for(let i = 0; i < total+1; i++) {
+            if(first === undefined && second === undefined) {
+                first = numArr.shift();
+                second = numArr.shift();
+                operator = opArr.shift();
+            } else if(second === undefined) {
+                break;
+            } else if (first === 'Oh, a wise guy, eh?') {
+                break;
+            } else {
+                if(first.toString().indexOf('.') < 0) {
+                    first = Number.parseInt(first, 10);
+                } else {
+                    first = Number.parseFloat(first, 10);
+                }
+                if(second.toString().indexOf('.') < 0) {
+                    second = Number.parseInt(second, 10);
+                } else {
+                    second = Number.parseFloat(second, 10);
+                }
+                    first = operate(operator, first, second);
+                    second = numArr.shift();
+                    operator = opArr.shift();
+            }
+        }
+        if(first.toString().indexOf('.') > -1 && first.toString().length > 10) {
+            display.innerHTML = Number.parseFloat(first).toFixed(10).replace(/0+$/g, '');
+        } else {
+            display.innerHTML = first;
+        }
+    
+        evaluated = true;
+    }
+
+});
+
+backBtn.addEventListener('click', function() {
+    backspace();
+});
+
+decimalBtn.addEventListener('click', function() {
+    decimal();
+});
 
